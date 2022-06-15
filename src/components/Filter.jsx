ef753a404d6c94d2532f6d0421565a32c2a36732
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-06-15 21:14:42 +0200

    Added get request for genres

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,34 +1,31 @@
 import { Card, CardContent, CardActions, Typography, Grid} from '@mui/material';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import Genre from './Genre';
-
-const genresList = [
-    {
-        id: 1, 
-        name: "Action"
-    },
-    {
-        id: 2, 
-        name: "Adventure"
-    },
-    {
-        id: 3, 
-        name: "Animation"
-    }
-]
+import Constants from '../constants.json';
 
 const Filter = () => {
-    const [Genres, setGenres] = useState(genresList);
+    const [Genres, setGenres] = useState(null);
+
+    const genreUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + Constants.key + "&language=en-US";
+
+    useEffect(() => {
+        console.log("getting genres");
+        Axios.get(genreUrl).then((response) => {
+            setGenres(response.data.genres);
+            console.log(response.data.genres);
+        });
+    }, []);
 
     return (
-      <Card elevation={5}>
+      <Card elevation={5} sx={{ mr: 2 }}>
         <CardContent>
             <Typography variant="h6" align="left" sx={{ m: 1 }}>Filters</Typography>
             <Typography variant="subtitle1" align="left" sx={{ m: 1, borderTop: 1, borderColor: 'primary.light' }}>Genres</Typography>
         </CardContent>
         <CardActions>
             <Grid container spacing={1} direction="row">
-                {Genres.map((genre) => (<Genre key={genre.id} genre={genre} />))}
+                {Genres !== null ? Genres.map((genre) => (<Genre key={genre.id} genre={genre} />)) : ""}
             </Grid>
         </CardActions>
       </Card>
