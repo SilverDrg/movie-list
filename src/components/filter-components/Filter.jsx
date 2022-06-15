1a39fef92a426cc880d90b3ce85232fa4e2ea63d
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-06-16 00:09:00 +0200

    Added filter functionality

--- src/components/filter-components/Filter.jsx
+++ src/components/filter-components/Filter.jsx
@@ -1,13 +1,16 @@
-import { Card, CardContent, CardActions, Typography, Grid} from '@mui/material';
+import { Card, CardContent, CardActions, Typography, Grid, Button } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import React, { useState, useEffect, useContext } from 'react';
 import Axios from 'axios';
 import Genre from './Genre';
 import Constants from '../../constants.json';
 import { FilterContext } from '../context-components/FilterContextProvider';
+import { MovieContext } from '../context-components/MovieContextProvider';
 
 const Filter = () => {
     const [Genres, setGenres] = useState(null);
     const filterContext = useContext(FilterContext);
+    const movieContext = useContext(MovieContext);
 
     const genreUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + Constants.key + "&language=en-US";
 
@@ -16,7 +19,21 @@ const Filter = () => {
             setGenres(response.data.genres);
             console.log(response.data.genres);
         });
-    }, []);
+    }, [genreUrl]);
+
+    const SearchMovies = () => {
+        let moviesUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" + Constants.key + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+        if(filterContext.filters.category) {
+            let newUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" + Constants.key + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=";
+            filterContext.filters.category.forEach((genre) => {
+                newUrl += genre + "%2C";
+            });
+            moviesUrl = newUrl.slice(0, -3);
+            moviesUrl += "&with_watch_monetization_types=flatrate";
+        }
+
+        movieContext.dispatch({type: 'search', payload: moviesUrl });
+    }
 
     return (
       <Card elevation={5} sx={{ mr: 2, ml: 1 }}>
@@ -31,7 +48,7 @@ const Filter = () => {
             </Grid>
         </CardActions>
         <CardActions>
-            
+            <Button variant="contained" fullWidth="true" startIcon={ <SearchIcon/> } onClick={SearchMovies}>Search</Button>
         </CardActions>
       </Card>
     );
--- src/components/movie-components/Movies.jsx
+++ src/components/movie-components/Movies.jsx
@@ -1,30 +1,21 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Grid, Box } from '@mui/material';
-import { FilterContext } from '../context-components/FilterContextProvider';
+import { MovieContext } from '../context-components/MovieContextProvider';
 import Axios from 'axios';
 import Movie from './Movie';
 import Constants from '../../constants.json';
 
 const Movies = () => {
-    const [Movies, setMovies] = useState(null);
-    const filterContext = useContext(FilterContext);
-
-    let moviesUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" + Constants.key + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
-    if(filterContext.filters.category) {
-        let newUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" + Constants.key + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=";
-        filterContext.filters.category.forEach((genre) => {
-            newUrl += genre + "%2C";
-        });
-        moviesUrl = newUrl.slice(0, -3);
-        moviesUrl += "&with_watch_monetization_types=flatrate";
-    }
+    const [Movies, setMovies] = useState([]);
+    const movieContext = useContext(MovieContext);
 
     useEffect(() => {
-        Axios.get(moviesUrl).then((response) => {
+        console.log("movies url")
+        console.log(movieContext.movies)
+        Axios.get(movieContext.movies).then((response) => {
             setMovies(response.data.results);
-            console.log(response.data.genres);
         });
-    }, []);
+    }, [movieContext]);
 
     return (
       <Box component="main" maxWidth="xs">
