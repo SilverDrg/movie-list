345127fe46e206586158d63b100c3598ea39f955
Silver <matija.plevnik@gmail.com>
2022-06-16 20:46:16 +0200

    Changed filter flex size

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             <NavBar/>
             <Grid container spacing={4} justifyContent="center" sx={{ mt: 3 }}>
               <Grid item container xs={12} md={10}>
-                <Grid item xs={4} md={3} lg={2.5} xl={2} sx={{ mt: 1 }}>
+                <Grid item xs={4} md={3} lg={2.5} xl={2.5} sx={{ mt: 1 }}>
                   <Filter />
                 </Grid>
                 <Grid item xs={4} md={6} lg={8} xl={8} sx={{ mt: 1 }}>
