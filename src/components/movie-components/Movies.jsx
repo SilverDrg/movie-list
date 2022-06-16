4d2ff70b2d683d5a08ffe74593cc4a175e87c798
Silver <matija.plevnik@gmail.com>
2022-06-16 20:38:12 +0200

    Added event listener for scrolling

--- src/components/movie-components/Movies.jsx
+++ src/components/movie-components/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import { Grid, Box, Button } from '@mui/material';
 import { MovieContext } from '../context-components/MovieContextProvider';
 import Axios from 'axios';
@@ -7,16 +7,10 @@ import Movie from './Movie';
 const Movies = () => {
     const [Movies, setMovies] = useState([]);
     const [Page, setPage] = useState(2);
-    const listenerRef = useRef();
+    const [InfiniteLoad, setInfiniteLoad] = useState(false);
     const movieContext = useContext(MovieContext);
 
-    useEffect(() => {
-        Axios.get(movieContext.movies).then((response) => {
-            setMovies(response.data.results);
-        });
-    }, [movieContext]);
-
-    const LoadMore = () => {
+    const LoadMore = useCallback(() => {
       let moviesUrl = movieContext.movies;
       let newUrl = "";
       moviesUrl.split("&").map((text) => {
@@ -28,23 +22,39 @@ const Movies = () => {
         return text;
       })
       newUrl = newUrl.slice(0, -1);
+
       console.log(newUrl);
       Axios.get(newUrl).then((response) => {
         setMovies([...Movies, ...response.data.results]);
+        setInfiniteLoad(true);
       });
-    }
+    }, [Page, Movies, movieContext.movies]);
 
-    const onScroll = () => {
-      if (listenerRef.current) {
-        const { scrollTop, scrollHeight, clientHeight } = listenerRef.current;
-        if (scrollTop + clientHeight === scrollHeight) {
-          console.log("reached bottom");
+    useEffect(() => {
+      Axios.get(movieContext.movies).then((response) => {
+          setMovies(response.data.results);
+      });
+    }, [movieContext]);
+
+    useEffect(() => {
+      const ScrollLoad = (event) => {
+        const bottom = event.target.scrollingElement.clientHeight + event.target.scrollingElement.scrollTop >= event.target.scrollingElement.scrollHeight - 5;
+        console.log(InfiniteLoad);
+        if(bottom && InfiniteLoad){
+            LoadMore();
+            setInfiniteLoad(true);
         }
-      }
-    };
+      };
+
+      window.addEventListener('scroll', ScrollLoad);
+
+      return () => {
+        window.removeEventListener('scroll', ScrollLoad);
+      };
+    }, [InfiniteLoad, LoadMore]);
 
     return (
-      <Box component="main" maxWidth="xs" onScroll={onScroll}>
+      <Box component="main" maxWidth="xs">
         <Grid container spacing={10} justifyContent="center" alignItems="stretch" direction="row">
           {Movies.map((movie) => (<Movie key={movie.id} movie={movie}/>))}
         </Grid>
