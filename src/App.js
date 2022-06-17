7c989bdcd1301eda84ca8bf8cf3327b18ef9ab6e
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-06-17 19:03:55 +0200

    Added title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { CssBaseline, Box, Grid } from '@mui/material';
+import { CssBaseline, Box, Grid, Typography } from '@mui/material';
 import { NavBar, Filter, Movies } from './components';
 import { FilterContextProvider, MovieContextProvider } from './components';
 import './App.css';
@@ -13,6 +13,7 @@ function App() {
             <NavBar/>
             <Grid container spacing={1} justifyContent="center" sx={{ mt: 3, ml: 'auto' }}>
               <Grid item container justifyContent="flex-start" xs={12} md={11} lg={10} xl={9}>
+                <Typography variant="h5" align="left" sx={{ width: '100%', mb: 1 }}><b>Popular Movies</b></Typography>
                 <Grid item xs={'auto'} md={'auto'} lg={'auto'} sx={{ mt: 1, mr: 1, minWidth: '260px', width: '260px' }}>
                   <Filter />
                 </Grid>
