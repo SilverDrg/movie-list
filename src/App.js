1088b4458648a59d548141d354bc86d9016d2fa4
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-06-17 18:57:24 +0200

    Updated grid styling

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
           <CssBaseline/>
           <Box>
             <NavBar/>
-            <Grid container spacing={1} justifyContent="center" sx={{ mt: 3 }}>
-              <Grid item container justifyContent="flex-start" xs={12} md={10}>
-                <Grid item xs={4} md={2} lg={2.5} sx={{ mt: 1, mr: 1, minWidth: '260px', width: '260px' }}>
+            <Grid container spacing={1} justifyContent="center" sx={{ mt: 3, ml: 'auto' }}>
+              <Grid item container justifyContent="flex-start" xs={12} md={11} lg={10} xl={9}>
+                <Grid item xs={'auto'} md={'auto'} lg={'auto'} sx={{ mt: 1, mr: 1, minWidth: '260px', width: '260px' }}>
                   <Filter />
                 </Grid>
-                <Grid item xs={3} md={6} lg={8.5} xl={9} sx={{ mt: 1 }}>
+                <Grid item xs={8} md={8} lg={8.5} xl={9} sx={{ mt: 1 }}>
                   <Movies />
                 </Grid>
               </Grid>
